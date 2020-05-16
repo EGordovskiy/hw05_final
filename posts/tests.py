bcbd5e3493474e9079ec9de0bdc2bc4d3c1e1d8b
Егор Гордовский <stepnoy-volk@yandex.ru>
2020-05-16 03:34:18 +0300

    Commit for Tests

--- posts/tests.py
+++ posts/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.core.files.images import ImageFile
 from django.core.cache.utils import make_template_fragment_key
 from django.core.cache import cache
+from .models import Follow, Comment
 
 
 class TestAutorizedUser(TestCase):
@@ -109,8 +110,8 @@ class TestCache(TestCase):
 class TestImages(TestCase):
     testusername = 'userimage'
     testtext1 = 'testtext1'
+    testtext2 = 'testtext2'
     testemail = 'image@ya.ru'
-    #grouptest = 'testgroup'
     password = 'yac@q765490'
     client = Client()
     def setUp(self):
@@ -122,31 +123,89 @@ class TestImages(TestCase):
         self.response_login = self.client.post(
                 "/auth/login/", {"username": self.testusername, "password": self.password}, follow=True
             )
-
-        self.grouptest = self.client.group()
-
-        with open('media/posts/ozero.jpg', 'rb') as fp:
-            self.post_create_image = self.client.post(
-                    "/new/", {"text": self.testtext1, "group": self.grouptest, "image": fp}, follow=True
-                )
-
-            self.response_profile = self.client.get("/userimage/")
-            self.response_post = self.client.get("/userimage/1/")
-            self.response_index = self.client.get("/")
-            self.response_group = self.client.get("/group/testgroup/")
+        #Создают новый пост
+        self.response_create_post = self.client.post("/new/", {"text": self.testtext1}, follow=True)
+        self.response_profile = self.client.get("/userimage/")
+        self.response_post = self.client.get("/userimage/1/")
+        self.response_index = self.client.get("/")
         
     def test_post_with_image(self):
-        #Проверяем страницу конкретной записи с картинкой на наличие тега <img>
-        #self.assertContains(self.response_post, "<img")
-        self.assertEqual(self.post_create_image.status_code, 200)
+        # Редактирую пост и вставляю в него картинку
+        self.assertEqual(self.response_create_post.status_code, 200)
+        with open('media/posts/ozero.jpg', 'rb') as image:
+            post_edit = self.client.post(
+                    "/userimage/1/edit", {"text": self.testtext2, "image": image}
+                )
+        # На всякий случай проверяю, отредактировался ли пост
+        self.assertEqual(post_edit.status_code, 301)
+        # Проверяю наличие тега "<img" на странице отредактированного поста с кодом ответа 200
+        self.assertContains(self.response_post, "<img", status_code=200)
     
     def test_have_images(self):
-        self.assertContains(self.response_profile, "<img")
-        self.assertContains(self.response_index, "<img")
-        self.assertContains(self.response_group, "<img")
+        # Проверяю наличие тега "<img" на страницах index & profile с кодом ответа 200
+        self.assertContains(self.response_profile, "<img", status_code=200)
+        self.assertContains(self.response_index, "<img", status_code=200)
 
     def test_safe_download(self):
-        with open('media/posts/text_file.txt', 'rb') as fp:
-            response = self.client.post('/new/', {'text': self.testtext1, 'image': fp}, follow=True)
-            self.assertContains(response, 'Upload a valid image. The file you uploaded was either not an image or a corrupted image.')
+        # Создаю пост, но вместо картинки пробую приложить текстовый документ, получаю ошибку об этом
+        with open('media/posts/text_file.txt', 'rb') as file:
+            response = self.client.post('/new/', {'text': self.testtext1, 'image': file})
+            self.assertFormError(response, 'form', 'image', 'Upload a valid image. The file you uploaded was either not an image or a corrupted image.')
 
+
+
+class TestFollow(TestCase):
+    testusername1 = 'username1'
+    testusername2 = 'username2'
+    testtext1 = 'testtext1'
+    testtext2 = 'testtext2'
+    testemail1 = 'test1@ya.ru'
+    testemail2 = 'test2@ya.ru'
+    password = 'yac@q765490'
+    client = Client()
+
+    def setUp(self):
+        #Создаем пользователя 1
+        self.response_signup = self.client.post(
+                "/auth/signup/", {"username": self.testusername1, "email": self.testemail1, 
+                "password1": self.password, "password2": self.password}, follow=True
+            )
+        self.response_login = self.client.post(
+                "/auth/login/", {"username": self.testusername1, "password": self.password}, follow=True
+            )
+        # Создаем пост
+        self.response_create_post = self.client.post("/new/", {"text": self.testtext1}, follow=True)
+        #Создаем пользователя 2
+        self.response_signup = self.client.post(
+                "/auth/signup/", {"username": self.testusername2, "email": self.testemail2, 
+                "password1": self.password, "password2": self.password}, follow=True
+            )
+        self.response_login = self.client.post(
+                "/auth/login/", {"username": self.testusername2, "password": self.password}, follow=True
+            )
+        
+
+    def test_follow_unfollow(self):
+        self.client.get("/username1/follow/", follow=True)
+        follow_count = Follow.objects.count()
+        self.assertEqual(follow_count, 1)
+        self.client.get("/username1/unfollow/", follow=True)
+        unfollow_count = Follow.objects.count()
+        self.assertEqual(unfollow_count, 0)
+
+
+    def test_new_post_in_follow_index(self):
+        self.client.get('/username1/follow/', follow=True)
+        response_follow_index = self.client.get('/follow/')
+        self.assertContains(response_follow_index, self.testtext1, status_code=200)
+        self.client.get('/username1/unfollow/', follow=True)
+        response_unfollow_index = self.client.get('/follow/')
+        self.assertNotContains(response_unfollow_index, self.testtext1, status_code=200)
+
+
+    def test_create_comment_for_post(self):
+        self.client.post("/username1/1/comment/", {"text": self.testtext2}, follow=True)
+        response = self.client.get("/username1/1/")
+        comment_count = Comment.objects.filter(post=1).count()
+        self.assertEqual(comment_count, 1)
+        self.assertContains(response, self.testtext2, status_code=200)
